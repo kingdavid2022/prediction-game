d7a4f5bc39a978fe4bf41c7694ea24a754642944
Sharath <sharathkrml@gmail.com>
2022-05-26 11:46:34 +0530

    fetch balance too

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,13 @@ import PredictionCard from "../components/PredictionCard";
 import { Tabs } from "../constants/types";
 
 import Web3Modal from "web3modal";
-import { providers, Contract } from "ethers";
+import { providers, Contract, utils } from "ethers";
 import { TOKEN_ABI, TOKEN_ADDRESS } from "../constants/contract";
 
 const Home: NextPage = () => {
   const [tab, setTab] = useState<Tabs>("contest");
   const [walletConnected, setWalletConnected] = useState(false);
+  const [minting, setMinting] = useState(false);
   const web3ModalRef = useRef<any>();
   const connectWallet = async () => {
     try {
@@ -22,14 +23,38 @@ const Home: NextPage = () => {
       console.error(err);
     }
   };
-  const getFirstTimeOrNot = async () => {
+  const getFirstTimeOrNotAndBalance = async () => {
     try {
       const signer = await getProviderOrSigner(true);
       const tokenContract = new Contract(TOKEN_ADDRESS, TOKEN_ABI, signer);
       // Get the address associated to the signer which is connected to  MetaMask
       const address = await signer.getAddress();
       let notFirstTime = await tokenContract.notFirstTime(address);
+      let balance = await tokenContract.balanceOf(address);
       console.log(notFirstTime);
+      console.log("balance=>",balance);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const mintToken = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const tokenContract = new Contract(TOKEN_ADDRESS, TOKEN_ABI, signer);
+      // Get the address associated to the signer which is connected to  MetaMask
+      const address = await signer.getAddress();
+      let notFirstTime = await tokenContract.notFirstTime(address);
+      let txn;
+      if (notFirstTime) {
+        txn = await tokenContract.mint({
+          value: utils.parseEther("1.0"),
+        });
+      } else {
+        txn = await tokenContract.mint();
+      }
+      setMinting(true);
+      await txn.wait();
+      setMinting(false);
     } catch (err) {
       console.error(err);
     }

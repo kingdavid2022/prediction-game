77d3ef300e4a969925cae6f57621c961f87d3fd6
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2022-05-26 16:20:51 +0530

    added function gettime

--- components/PredictionPage.tsx
+++ components/PredictionPage.tsx
@@ -7,6 +7,41 @@ import { PredictionPageProps } from "../constants/types";
 const PredictionPage = ({ onclick, currentPrice, nextContextTime }:PredictionPageProps) => {
   const [price, setprice] = useState('')
   const [token, settoken] = useState('')
+  const getTime = () =>{
+    
+    var countDownDate = new Date(nextContextTime).getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Display the result in the element with id="demo"
+      console.log(days + "d " + hours + "h " + minutes + "m " + seconds + "s ")
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(x);
+      }
+    }, 1000);
+  }
+
+  useEffect(() => {
+    
+  getTime();
+  }, [])
+  
+
   return (
     <div className="absolute w-screen h-screen flex flex-col items-center justify-center bg-image">
       <Head>
